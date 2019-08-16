e316bf1e1777e09efe499ec0d2c4543011e0ecda
Edgar <westerntrojan@gmail.com>
2019-08-17 02:39:09 +0500

    added-hot-cache

--- src/index.js
+++ src/index.js
@@ -8,5 +8,3 @@ render(<App />, document.getElementById('root'));
 if (module.hot) {
 	module.hot.accept();
 }
-
-console.log(process.env.TEST);
--- webpack.config.js
+++ webpack.config.js
@@ -5,33 +5,43 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const isDev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
 	entry: path.resolve(__dirname, 'src', 'index.js'),
 
 	output: {
 		path: path.resolve(__dirname, 'build'),
-		filename: 'bundle.js',
+		filename: '[name].[hash].js',
 	},
 
 	devServer: {
 		port: 3000,
 		compress: true,
-		historyApiFallback: true,
+		historyApiFallback: false,
 		inline: true,
 		progress: true,
-		clientLogLevel: 'none',
-		contentBase: path.resolve(__dirname, 'src'),
-		watchContentBase: true,
 		publicPath: '/',
 		hot: true,
-		quiet: true,
 	},
 
 	devtool: 'source-map',
 
 	optimization: {
+		moduleIds: 'hashed',
+		runtimeChunk: 'single',
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendors',
+					chunks: 'all',
+				},
+			},
+		},
 		minimizer: [
 			new TerserJSPlugin({
 				cache: true,
@@ -63,11 +73,16 @@ module.exports = {
 			filename: 'style.css',
 		}),
 		new Dotenv(),
+		new HardSourceWebpackPlugin(),
 		// new BundleAnalyzerPlugin()
 	],
 
 	resolve: {
 		extensions: ['.js', '.jsx'],
+		alias: {
+			'~': path.resolve(__dirname, '/'),
+			'&': path.resolve(__dirname, 'src/'),
+		},
 	},
 
 	module: {
@@ -75,19 +90,37 @@ module.exports = {
 			{
 				test: /\.(js|jsx)$/,
 				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-				},
+				use: [
+					{
+						loader: 'thread-loader',
+					},
+					{
+						loader: 'babel-loader',
+					},
+				],
 			},
 
 			{
 				test: /\.scss$/,
-				use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+				use: [
+					isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+					},
+					{
+						loader: 'sass-loader',
+					},
+				],
 			},
 
 			{
 				test: /\.css$/,
-				use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader'],
+				use: [
+					isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+					},
+				],
 			},
 		],
 	},
